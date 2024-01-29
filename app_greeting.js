6f89a48ee281a2753916ad71d7dcccba6d5bdf1c
MuhammadHunain0314 <156586251+MuhammadHunain0314@users.noreply.github.com>
2024-01-29 12:56:00 +0500

    Add files via upload

---
+++ app_greeting.js
@@ -0,0 +1,7 @@
+
+let userName = prompt("Enter a name");
+function usergreet(name) {
+    return "Asalam u alikum, " + name + " I hope you are doing well and good,Welcome to our page";
+}
+let result = usergreet(userName);
+console.log(result);
